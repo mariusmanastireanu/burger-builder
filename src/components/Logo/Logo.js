778c97b7ba768e86f9a6ee7de0110800042b14b7
Marius Manastireanu <marius.manastireanu@gmail.com>
2022-01-06 12:45:04 +0200

    Added logo image

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import burgerLogo from "../../images/burger-logo.png"
+import classes from "./Logo.css"
+
+const logo = (props) => (
+
+    <div className="Logo">
+        <img src={burgerLogo}/>
+    </div>
+
+);
+
+export default logo;
